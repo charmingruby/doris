6da0911a24240e7e0a80d2ec3d633ca7b3b086e8
charmingruby <mrrubycanal3105@gmail.com>
2025-04-20 10:22:52 -0300

    feat: rest server

---
+++ lib/delivery/rest/server.go
@@ -0,0 +1,41 @@
+package rest
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	srv *http.Server
+}
+
+func NewServer(host, port string) (*Server, *gin.Engine) {
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.Default()
+
+	r.Use(gin.Recovery())
+	r.Use(gin.Logger())
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    addr,
+	}
+
+	return &Server{
+		srv: srv,
+	}, r
+}
+
+func (s *Server) Start() error {
+	return s.srv.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.srv.Shutdown(ctx)
+}
--- service/gateway/cmd/api/main.go
+++ service/gateway/cmd/api/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/charmingruby/doris/lib/delivery/rest"
 	"github.com/charmingruby/doris/lib/instrumentation/logger"
 	"github.com/charmingruby/doris/service/gateway/config"
 )
@@ -21,5 +26,27 @@ func main() {
 
 	log.Info("config loaded successfully")
 
-	fmt.Printf("config: %+v\n", cfg)
+	server, _ := rest.NewServer(cfg.Custom.RestServerHost, cfg.Custom.RestServerPort)
+
+	go func() {
+		if err := server.Start(); err != nil {
+			log.Error("failed to start rest server", "error", err)
+			return
+		}
+	}()
+
+	gracefulShutdown(log, server)
+}
+
+func gracefulShutdown(log *logger.Logger, srv *rest.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to shutdown rest server", "error", err)
+	}
 }
