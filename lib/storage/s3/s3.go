260e7a05f2d7fefd37e8ac1ac6031b62025099d8
charmingruby <mrrubycanal3105@gmail.com>
2025-05-15 13:35:51 -0300

    feat: s3 client connection

--- lib/storage/s3/s3.go
+++ lib/storage/s3/s3.go
@@ -1,4 +1,4 @@
-package dynamo
+package s3
 
 import (
 	"context"
@@ -6,12 +6,12 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/charmingruby/doris/lib/instrumentation"
 )
 
 type Client struct {
-	Client *dynamodb.Client
+	Client *s3.Client
 	logger *instrumentation.Logger
 }
 
@@ -19,14 +19,12 @@ func New(logger *instrumentation.Logger, region string) (*Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	cfg, err := config.LoadDefaultConfig(ctx)
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
 	if err != nil {
 		return nil, fmt.Errorf("failed to load configuration, %v", err)
 	}
 
-	cfg.Region = region
-
-	client := dynamodb.NewFromConfig(cfg)
+	client := s3.NewFromConfig(cfg)
 
 	return &Client{
 		Client: client,
