f7c319b767535f9c6cdd5c8fffb1cfdf38185f09
charmingruby <mrrubycanal3105@gmail.com>
2025-05-19 13:00:46 -0300

    chore: fixes ollama models setup

--- service/codex/script/setup-ollama-models.sh
+++ service/codex/script/setup-ollama-models.sh
@@ -2,4 +2,8 @@
 
 echo "Pulling nomic-embed-text model..."
 docker exec doris-codex-ollama ollama pull nomic-embed-text
+
+echo "Pulling tinyllama model..."
+docker exec doris-codex-ollama ollama pull tinyllama
+
 echo "Done!" 
